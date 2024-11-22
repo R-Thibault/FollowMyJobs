5f63fdcb436f159658cdf23ff10ac1730fad878c
R-Thibault <thibault.rossa@gmail.com>
2024-11-22 16:38:41 +0100

    6-implement-user-registration-endpoint -- Define the registration route in route/routes.go

--- backend/generate_mocks.sh
+++ backend/generate_mocks.sh
@@ -4,10 +4,14 @@ echo "Generating mocks..."
 # Define the directories to watch for changes
 WATCH_DIRS=(
     "repository/user_repository"
+    "repository/otp_repository"
     "services/user_services"
     "services/token_services"
+    "services/otp_services"
+    "services/registration_services"
     "internal/hash_util"
     "internal/tokenGenerator_util"
+    "internal/otpGenerator_util"
 )
 
 # Define a function to generate mocks for specific interfaces
@@ -17,6 +21,10 @@ generate_mock() {
             echo "Generating mock for UserRepositoryInterface..."
             mockery --name UserRepositoryInterface --dir ./repository/user_repository --output ./repository/mocks
             ;;
+        "repository/otp_repository")
+            echo "Generating mock for OTPRepositoryInterface..."
+            mockery --name OTPRepositoryInterface --dir ./repository/otp_repository --output ./repository/mocks
+            ;;
         "services/user_services")
             echo "Generating mock for UserServiceInterface..."
             mockery --name UserServiceInterface --dir ./services/user_services --output ./services/mocks
@@ -25,6 +33,14 @@ generate_mock() {
             echo "Generating mock for TokenServiceInterface..."
             mockery --name TokenServiceInterface --dir ./services/token_services --output ./services/mocks
             ;;
+        "services/otp_services")
+            echo "Generating mock for OTPServiceInterface..."
+            mockery --name OTPServiceInterface --dir ./services/otp_services --output ./services/mocks
+            ;;
+        "services/registration_services")
+            echo "Generating mock for OTPServiceInterface..."
+            mockery --name OTPServiceInterface --dir ./services/registration_services --output ./services/mocks
+            ;;
         "internal/hash_util")
             echo "Generating mock for HashingServiceInterface..."
             mockery --name HashingServiceInterface --dir ./internal/hash_util --output ./internal/mocks
@@ -32,6 +48,10 @@ generate_mock() {
         "internal/tokenGenerator_util")
             echo "Generating mock for JWTTokenGeneratorUtilInterface..."
             mockery --name JWTTokenGeneratorUtilInterface --dir ./internal/tokenGenerator_util --output ./internal/mocks
+            ;;
+        "internal/otpGenerator_util")
+            echo "Generating mock for OtpGeneratorServiceInterface..."
+            mockery --name OtpGeneratorServiceInterface --dir ./internal/otpGenerator_util --output ./internal/mocks
             ;;
         *)
             echo "No matching file for mock generation."
