76ef63f0b809ed37e30cfd9161c005bb816feec6
R-Thibault <thibault.rossa@gmail.com>
2025-01-04 09:37:14 +0100

    32-setup-frontend-with-nextjs-15-tailwind-css-and-shadcn -- feat: add nextJS with Tailwind, shadcn and next-intl for translation Fr/En

---
+++ frontend/src/app/[locale]/layout.tsx
@@ -0,0 +1,56 @@
+import { Metadata } from "next";
+import { metadataFr, metadataEn } from "../../metadata/metadata";
+import "./globals.css";
+import { routing } from "@/i18n/routing";
+import { notFound } from "next/navigation";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  const { locale } = await params;
+
+  if (locale === "fr") {
+    return metadataFr;
+  }
+  return metadataEn; // Default to English metadata for non-French locales
+}
+
+export default async function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: {
+    locale: string;
+  };
+}>) {
+  const { locale } = await params;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <head>
+        {/* Favicon links */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* <link rel="icon" type="image/png" sizes="192x192" href="/icon.png" />
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" /> */}
+      </head>
+      <body className="mx-auto min-h-screen ">
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
